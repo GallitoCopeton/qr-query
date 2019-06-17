a8d9d79c9d0936ceff6b8ed88bef7673912a7160
gabriel.michel <gabriel.michel@unimabi.local>
2019-06-17 15:34:40 -0500

    Read image script with only necessary functions

---
+++ read_image.py
@@ -0,0 +1,47 @@
+import base64
+import os
+
+import cv2
+import numpy as np
+import pymongo
+from matplotlib import pyplot as plt
+
+
+def read_from_DB(db, qr, array=True, count=0):
+    """
+    This function will read images from a database, local or remote.
+    """
+    if array:
+        cursor = db.imagestotals.find({'fileName': qr})
+        if cursor.count() > 0:
+            return make_image_list(cursor)
+        else:
+            return 0
+    else:
+        cursor = db.imagestotals.find_one({'fileName': qr, 'count': count})
+        if cursor:
+            return readb64(cursor['file'])
+        else:
+            return 0
+
+
+def readb64(base64_string):
+    """
+    This function will decode images from base 64 to matrix form.
+    """
+    nparr = np.fromstring(base64.b64decode(base64_string, '-_'), np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    rows, cols, _ = image.shape
+    if(rows < cols):
+        image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+    return image
+
+
+def make_image_list(cursor):
+    """
+    This function will arange a list of decoded images from a Mongo cursor.
+    """
+    images = []
+    for entry in cursor:
+        images.append(readb64(entry['file']))
+    return images
