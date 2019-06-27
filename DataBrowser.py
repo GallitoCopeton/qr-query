9170d694ffae4c30f009ea04cf2090d8665496a7
gabriel.michel <gabriel.michel@unima.com.mx>
2019-06-27 09:50:08 -0500

    Completed qr query (not in hub) and notebook with call to function

--- DataBrowser.py
+++ DataBrowser.py
@@ -30,8 +30,10 @@ class DataBrowser(Browser):
                 'location': self.LOCATION
             }
         geolocator = Nominatim(user_agent='Query')
-        result_dataframe = pd.DataFrame()
+        list_dicts = []
+        default_header = ['No. Imagen', 'Validez', 'Fecha + 5hrs', 'Ciudad']
         for document in qr_documents:
+            temporary_dict = {}
             # Location extraction
             latitude = str(document['location'][0]['latitude'])
             longitude = str(document['location'][0]['longitude'])
@@ -42,16 +44,22 @@ class DataBrowser(Browser):
                 'Ciudad': location.raw['address']['city'],
                 'Validez': document['control'],
                 'No. Imagen': document['count'],
-                'Fecha + 5hrs': str(document['createdAt'])
+                'Fecha + 5hrs': str(document['createdAt'])[0:-10]
             }
+            names_proteins = []
             for marker in document['marker']:
+                names_proteins.append(marker['name'])
+                header = default_header + names_proteins
                 temporary_dict[marker['name']] = marker['result']
-            # Update dataframe
-            result_dataframe = result_dataframe.append(
-                temporary_dict, ignore_index=True)
-            display(result_dataframe)
-            return {
-                'timeout': self.timeout,
-                'found': self.found,
-                'location': self.LOCATION
-            }
+            list_dicts.append(temporary_dict)
+        # Update dataframe
+        result_dataframe = pd.DataFrame.from_dict(list_dicts)
+        result_dataframe = result_dataframe[header]
+        result_dataframe = result_dataframe.sort_values(
+            by=['No. Imagen'], ascending=True)
+        display(result_dataframe)
+        return {
+            'timeout': self.timeout,
+            'found': self.found,
+            'location': self.LOCATION
+        }
--- qr_query.py
+++ qr_query.py
@@ -15,8 +15,8 @@ from ImageBrowser import ImageBrowser
 URI1 = 'mongodb://findOnlyReadUser:RojutuNHqy@idenmon.zapto.org:888/?authSource=prodLaboratorio'
 URI2 = 'mongodb+srv://findOnlyReadUser:RojutuNHqy@clusterfinddemo-lwvvo.mongodb.net/datamap?retryWrites=true'
 # Testing
-QR = '601170500100387'
-PATH = './'
+QR = '601170500100126'
+# PATH = './'
 
 # Check type of connection to databases.
 if URI1:
@@ -45,13 +45,13 @@ if LOCAL or CLOUD:
     if local_image_result['timeout']:
         print('Server is unreachable, timeout error.')
     if not local_image_result['found']:
-        print('No results were found locally, searching in the cloud now.')
+        print('No images were found locally, searching in the cloud now.')
         cloud_image_browser = ImageBrowser(QR, DB2, 'cloud')
         cloud_image_result = cloud_image_browser.browse_images()
         if cloud_image_result['timeout']:
             print('Mongo Atlas is unreachable, timeout error.')
         if not cloud_image_result['found']:
-            print('No results were found in Atlas.')
+            print('No images were found in Atlas.')
 else:
     print('No connection was successful, no information could be gathered.')
 
@@ -62,10 +62,62 @@ if LOCAL or CLOUD:
     if local_data_result['timeout']:
         print('Server is unreachable, timeout error')
     if not local_data_result['found']:
-        print('No results were found locally, searching in the cloud now.')
+        print('No data was found locally, searching in the cloud now.')
         cloud_data_browser = DataBrowser(QR, DB2, 'cloud')
         cloud_data_result = cloud_data_browser.browse_data()
         if cloud_data_result['timeout']:
             print('Mongo Atlas is unreachable, timeout error.')
         if not cloud_data_result['found']:
-            print('No results were found in Atlas.')
+            print('No data was found in Atlas.')
+
+def qr_search(QR_code):
+    if URI1:
+        try:
+            CLIENT1 = pymongo.MongoClient(URI1)
+            DB1 = CLIENT1.prodLaboratorio
+            LOCAL = True
+            print('Connection to local Mongo successful.')
+        except pymongo.errors.InvalidURI:
+            print('Could not connect to local Mongo, incorrect URI.')
+            LOCAL = False
+    if URI2:
+        try:
+            CLIENT2 = pymongo.MongoClient(URI2)
+            DB2 = CLIENT2.datamap
+            CLOUD = True
+            print('Connection to Atlas successful.')
+        except pymongo.errors.InvalidURI:
+            print('Could not connect to Atlas, incorrect URI.')
+            CLOUD = False
+            
+    # Gather images. Priority on local storage.
+    if LOCAL or CLOUD:
+        local_image_browser = ImageBrowser(QR, DB1, 'local')
+        local_image_result = local_image_browser.browse_images()
+        if local_image_result['timeout']:
+            print('Server is unreachable, timeout error.')
+        if not local_image_result['found']:
+            print('No images were found locally, searching in the cloud now.')
+            cloud_image_browser = ImageBrowser(QR, DB2, 'cloud')
+            cloud_image_result = cloud_image_browser.browse_images()
+            if cloud_image_result['timeout']:
+                print('Mongo Atlas is unreachable, timeout error.')
+            if not cloud_image_result['found']:
+                print('No images were found in Atlas.')
+    else:
+        print('No connection was successful, no information could be gathered.')
+
+    # Gather data. Priority on local storage
+    if LOCAL or CLOUD:
+        local_data_browser = DataBrowser(QR, DB1, 'local')
+        local_data_result = local_data_browser.browse_data()
+        if local_data_result['timeout']:
+            print('Server is unreachable, timeout error')
+        if not local_data_result['found']:
+            print('No data was found locally, searching in the cloud now.')
+            cloud_data_browser = DataBrowser(QR, DB2, 'cloud')
+            cloud_data_result = cloud_data_browser.browse_data()
+            if cloud_data_result['timeout']:
+                print('Mongo Atlas is unreachable, timeout error.')
+            if not cloud_data_result['found']:
+                print('No data was found in Atlas.')
